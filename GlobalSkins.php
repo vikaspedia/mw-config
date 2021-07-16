9d0edcd91a98e8de7316505e3cf9a41a171f57d4
vikaspediadev <87364735+vikaspediadev@users.noreply.github.com>
2021-07-17 02:24:59 +0700

    Create GlobalSkins.php

---
+++ GlobalSkins.php
@@ -0,0 +1,13 @@
+<?php
+wfLoadSkins( [
+    'MonoBook',
+    'Vector',
+    'Timeless',
+    'MinervaNeue'
+] );
+// Choose one of these and add chosen code to bottom of LocalSettings.php file:
+
+
+//$wgMFDefaultSkinClass = "SkinMinerva";
+//$wgDefaultSkin = 'minerva';
+//$wgMFAutodetectMobileView = true;
