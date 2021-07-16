f678a739c226bee22419cf139cca7560ee7194f3
Skh sourav halder <54745263+Skhsouravhalder@users.noreply.github.com>
2021-07-17 01:45:44 +0600

    Update InitialiseSettings.php
    Add test wiki group

--- InitialiseSettings.php
+++ InitialiseSettings.php
@@ -431,19 +431,39 @@ $wgConf->settings = [
 			],
 		],
 	],
-	'wgRemoveGroups' => [
-		'default' => [
+	'Skhsblsb_testwiki' => [
 			'bureaucrat' => [
-				'bot',
+				'testgroup',
+				'bureaucrat',
 				'sysop',
-			],
-			'sysop' => [
-				'autopatrolled',
 				'confirmed',
+				'autopatrolled',
+				'rollbacker',
+			],
+			'burcrat' => [
+				'bot',
+				'bureaucrat',
+				'burcart',
+				'exampleuser',
+				'testgroup',
+				'sysop',
+				'confirmed',
+				'autopatrolled',
+				'rollbacker',
+			],
+			'steward' => [
+				'consul',
+				'bot',
+				'bureaucrat',
+				'burcart',
+				'exampleuser',
+				'testgroup',
+				'sysop',
+				'confirmed',
+				'autopatrolled',
 				'rollbacker',
 			],
 		],
-	],
 	'+wgGroupPermissions' => [
 		'default' => [
 			'*' => [
