92cb45499a3d3cd75b1c797515c5c87472646582
Skh sourav halder <54745263+Skhsouravhalder@users.noreply.github.com>
2021-08-21 10:43:46 +0600

    U

--- InitialiseSettings.php
+++ InitialiseSettings.php
@@ -969,3 +969,24 @@ $wgGroupPermissions['writer']['edit'] = true;
 $wgGroupPermissions['writer']['createpage'] = true;
 // Four days
 $wgAutoConfirmAge = 86400*4; 
+//Contact
+$wgContactConfig['default'] = array(
+	'RecipientUser' => 'WikiUser', // Must be the name of a valid account which also has a verified e-mail-address added to it.
+	'SenderName' => 'Contact Form on ' . $wgSitename, // "Contact Form on" needs to be translated
+	'SenderEmail' => null, // Defaults to $wgPasswordSender, may be changed as required
+	'RequireDetails' => true, // Either "true" or "false" as required
+	'IncludeIP' => true, // Either "true" or "false" as required
+	'MustBeLoggedIn' => true, // Check if the user is logged in before rendering the form
+	'AdditionalFields' => array(
+		'Text' => array(
+			'label-message' => 'emailmessage',
+			'type' => 'textarea',
+			'rows' => 20,
+			'required' => true,  // Either "true" or "false" as required
+		),
+	),
+        // Added in MW 1.26
+	'DisplayFormat' => 'table',  // See HTMLForm documentation for available values.
+	'RLModules' => array(),  // Resource loader modules to add to the form display page.
+	'RLStyleModules' => array(),  // Resource loader CSS modules to add to the form display page.
+);
